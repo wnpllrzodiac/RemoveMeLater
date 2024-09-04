1b8940827f8c18ca41e45d37f03f930d423f6108
Michael.Ma <guo.liang.ma@autodesk.com>
2024-09-04 15:15:14 +0800

    update

--- util/testfcm.py
+++ util/testfcm.py
@@ -1,7 +1,22 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import messaging
- 
+import google
+from google.oauth2 import service_account
+
+SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
+
+def _get_access_token():
+  """Retrieve a valid access token that can be used to authorize requests.
+
+  :return: Access token.
+  """
+  credentials = service_account.Credentials.from_service_account_file(
+    'XXX.json', scopes=SCOPES)
+  request = google.auth.transport.requests.Request()
+  credentials.refresh(request)
+  return credentials.token
+
 mytoken = 'TOKEN'
 
 cred = credentials.Certificate("XXX.json")
@@ -19,3 +34,6 @@ message = messaging.Message(
 response = messaging.send(message)
 # Response is a message ID string.
 print('Successfully sent message:', response)
+
+access_token = _get_access_token()
+print(access_token)
